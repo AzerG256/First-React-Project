daece16aa0a315f6df084da75906dc1a539a5bf3
AzerG256 <azergrassi6@example.com>
2024-10-07 23:39:30 +0100

    setting up the structure

---
+++ todo/src/components/taskList/taskList.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./taskList.css"
+
+function TaskList () {
+  return (
+    <div className='container'>
+        <h1>taskList</h1>
+    </div>
+  )
+}
+
+export default TaskList;
